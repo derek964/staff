afd0a600bfe6fd82b5dc15747ee9401d6c3a71e9
derek964 <zhaoboliang964@163.com>
2021-08-30 21:23:47 +0800

    添加base02导航，主要设置前端web页面

--- stumgr/views.py
+++ stumgr/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def stuinfo(request):
+    return render(request,'stuinfo.html')
+
+def selcourse(request):
+    return render(request,'selcourse.html')
+
+def queryscore(request):
+    return render(request,'queryscore.html')
+
+def mopasswd(request):
+    return render(request,'mopasswd.html')
