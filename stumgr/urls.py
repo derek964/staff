a5d255d5353e044c44147959a04350d0805a6c8a
derek964 <zhaoboliang964@163.com>
2021-08-31 21:10:16 +0800

    基本功能完成，可以登录不同类型账号，可以修改密码

--- stumgr/urls.py
+++ stumgr/urls.py
@@ -3,13 +3,13 @@ from stumgr.views import *
 
 urlpatterns = [
     url(r'^$', index),
-    url(r'^index/', index),
-    url(r'^stuinfo/', stuinfo),
-    url(r'^tchinfo/', tchinfo),
-    url(r'^editscore/', editscore),
-    url(r'^motchpasswd/', motchpasswd),
-    url(r'^selcourse/', selcourse),
-    url(r'^queryscore/', queryscore),
-    url(r'^mopasswd/', mopasswd),
-    url(r'^logout/', logout),
+    url(r'^index', index),
+    url(r'^stuinfo', stuinfo),
+    url(r'^tchinfo', tchinfo),
+    url(r'^editscore', editscore),
+    url(r'^motchpasswd', motchpasswd),
+    url(r'^selcourse', selcourse),
+    url(r'^queryscore', queryscore),
+    url(r'^mopasswd', mopasswd),
+    url(r'^logout', logout),
 ]
--- stumgr/views.py
+++ stumgr/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from stumgr.models import *
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
 def index(request):
@@ -14,25 +14,40 @@ def index(request):
             auth.login(request, userinfo)
             stuinfo = student.objects.filter(username=username)
             if stuinfo:
-                return HttpResponseRedirect('/stuinfo')
+                return HttpResponseRedirect('/stuinfo/')
             else:
-                return HttpResponseRedirect('/tchinfo')
+                return HttpResponseRedirect('/tchinfo/')
     return render(request, 'index.html')
 
 @login_required
+@permission_required('stumgr.change_student')
 def stuinfo(request):
     return render(request, 'stuinfo.html')
 
 @login_required
+@permission_required('stumgr.change_student')
 def selcourse(request):
     return render(request, 'selcourse.html')
 
 @login_required
+@permission_required('stumgr.change_student')
 def queryscore(request):
     return render(request, 'queryscore.html')
 
 @login_required
+@permission_required('stumgr.change_student')
 def mopasswd(request):
+    if request.method == 'POST' and request.POST:
+        oldpassword = request.POST['oldpassword']
+        newpassword1 = request.POST['newpassword1']
+        newpassword2 = request.POST['newpassword2']
+        username = request.user
+        userinfo = auth.authenticate(username=username, password=oldpassword)
+        if userinfo and newpassword1 == newpassword2:
+            user = User.objects.get(username=username)
+            user.set_password(newpassword2)
+            user.save()
+            return render(request, 'mopasswd.html')
     return render(request, 'mopasswd.html')
 
 def logout(request):
@@ -40,13 +55,27 @@ def logout(request):
     return HttpResponseRedirect('/')
 
 @login_required
+@permission_required('stumgr.change_teacher')
 def tchinfo(request):
     return render(request, 'tchinfo.html')
 
 @login_required
+@permission_required('stumgr.change_teacher')
 def editscore(request):
     return render(request, 'editscore.html')
 
 @login_required
+@permission_required('stumgr.change_teacher')
 def motchpasswd(request):
+    if request.method == "post" and request.POST:
+        oldpassword = request.POST['oldpassword']
+        newpassword1 = request.POST['newpassword1']
+        newpassword2 = request.POST['newpassword2']
+        username = request.user
+        userinfo = auth.authenticate(username=username, password=oldpassword)
+        if userinfo and newpassword1 == newpassword2:
+            user = User.objects.get(username=username)
+            user.set_password(newpassword2)
+            user.save()
+            return render(request, 'motchpasswd.html')
     return render(request, 'motchpasswd.html')
